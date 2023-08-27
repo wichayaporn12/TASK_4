819565f1099aff7dfdb06b552ab6d6cb4d65c695
wichayaporn12 <stu6510014116@sskru.ac.th>
2023-08-27 19:30:25 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+const person = {
+  name: "Mint",
+  hairColor: "Black",
+  age: 20,
+  eyeColor: "Darkbrown",
+  leg: 2,
+  hand: 2,
+  eye: 2,
+  nose: 1,
+  mouth: 1,
+  ear: 2,
+  sounding: "speak"
+};
+
+const dog = {
+  name: "Latte",
+  color: "Lightbrown",
+  age: 2,
+  eyeColor: "Brown",
+  leg: 4,
+  hand: 0,
+  eye: 2,
+  nose: 1,
+  mouth: 1,
+  ear: 2,
+  sounding: "bark,bay"
+};
+
+const cat = {
+  name: "Mocha",
+  color: "Cream",
+  age: 2,
+  eyeColor: "Darkbrown",
+  leg: 4,
+  hand: 0,
+  eye: 2,
+  nose: 1,
+  mouth: 1,
+  ear: 2,
+  sounding: "meow"
+};
